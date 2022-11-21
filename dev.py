532b4d605f42eb8879805ee19758f525429eac24
Riad8321 <112904701+Riad8321@users.noreply.github.com>
2022-11-21 21:55:33 +0100

    add comments

--- dev.py
+++ dev.py
@@ -4,37 +4,36 @@ import time
 import urllib.request
 
 
-
+# lancer la fenêtre de cpature vidéo
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FPS,12)
 
+# detecter le qr code avec la webcam
 detector = cv2.QRCodeDetector()
 
+# fonction pour télécharger un fichier pdf
 def download_file(download_url, filename):
             response = urllib.request.urlopen(download_url)    
             file = open(filename + ".pdf", 'wb')
             file.write(response.read())
             file.close()
 
+# la liste des urls scannés sont stocké dans cette liste  
 liste_urls = [""]
 
 while True:
     check, img = cap.read()
     data2, bbox, _ = detector.detectAndDecode(img)
-
-   # if(bbox is not None):
-      #  for i in range (len(bbox)):
-      #      cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color = (0, 0, 255), thickness = )
-       #     cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 0, 0), 2)   
     if data2:
         data = data2
+        # vérifie si l'url scanné n'est pas dans la liste
         if data not in liste_urls:
             print(data)
             print(liste_urls)
             pdf_path = str(data)
             download_file(pdf_path, "Test2")
             liste_urls.extend([data])
-            os.system("lpr -P Canon_MG5700_series test2.pdf")
+            os.system("lpr -P Canon_MG5700_series file_Downloaded.pdf")
             os.remove("file_Downloaded.pdf")
     
     cv2.imshow("image", img)
